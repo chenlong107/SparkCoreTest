b7813602953c99fd647733ee36623a86d350ce01
chenlong <chenlong@qq.com>
2020-10-28 16:23:39 +0800

    hotfix first commit

---
+++ src/main/java/test3.java
@@ -0,0 +1,8 @@
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+        System.out.println("ddd");
+    }
+}
