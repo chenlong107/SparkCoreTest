ac149561ba0277f8996bc71b2ac6680d0695fbd0
chenlong <chenlong@qq.com>
2020-10-28 18:06:31 +0800

    master four commit for merge

--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -5,6 +5,6 @@ public class test2 {
         System.out.println("ccc");
         System.out.println("ddd");
         System.out.println("this is master");
-        System.out.println("this is third master's test for merge");
+        System.out.println("this is four master's test for merge");
     }
 }
